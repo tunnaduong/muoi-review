ec2570aa2cd880d5372497f1bf1b89523944bb4a
Tunna Duong <tunnaduong@gmail.com>
2023-10-22 23:15:55 +0700

    sxc

--- api/upload_image.php
+++ api/upload_image.php
@@ -1,6 +1,6 @@
 <?php
 if (!empty($_FILES['file_attachment']['name'])) {
-    $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/images/";
+    $target_dir = "../assets/images/";
     if (!file_exists($target_dir)) {
         mkdir($target_dir, 0777);
     }
