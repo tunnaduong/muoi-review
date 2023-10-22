fb265d03678ea677ee41f35e447546c2072cef83
Tunna Duong <tunnaduong@gmail.com>
2023-10-22 23:06:22 +0700

    sds

--- api/upload_image.php
+++ api/upload_image.php
@@ -5,7 +5,7 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
-if (isset($_POST["submit"])) {
+if (isset($_POST["photo"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if ($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
