0dbacaa5bc4d9542db30ea0f4446082d65c90687
Tunna Duong <tunnaduong@gmail.com>
2023-10-22 23:14:44 +0700

    sdsd

--- api/upload_image.php
+++ api/upload_image.php
@@ -1,51 +1,56 @@
 <?php
-$target_dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/images/";
-// echo $target_dir;
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-// Check if image file is a actual image or fake image
-if (isset($_POST["photo"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if ($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+if (!empty($_FILES['file_attachment']['name'])) {
+    $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/images/";
+    if (!file_exists($target_dir)) {
+        mkdir($target_dir, 0777);
     }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Allow certain file formats
-if (
-    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif"
-) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo json_encode(array("status" => "success", "message" => "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.", "path" => "/assets/images/" . htmlspecialchars(basename($_FILES["fileToUpload"]["name"]))));
+    $target_file =
+        $target_dir . basename($_FILES["file_attachment"]["name"]);
+    $imageFileType =
+        strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo json_encode(
+            array(
+                "status" => 0,
+                "data" => array(), "msg" => "Sorry, file already exists."
+            )
+        );
+        die();
+    }
+    // Check file size
+    if ($_FILES["file_attachment"]["size"] > 50000000) {
+        echo json_encode(
+            array(
+                "status" => 0,
+                "data" => array(),
+                "msg" => "Sorry, your file is too large."
+            )
+        );
+        die();
+    }
+    if (
+        move_uploaded_file(
+            $_FILES["file_attachment"]["tmp_name"],
+            $target_file
+        )
+    ) {
+        echo json_encode(
+            array(
+                "status" => 1,
+                "data" => array(),
+                "msg" => "The file " .
+                    basename($_FILES["file_attachment"]["name"]) .
+                    " has been uploaded."
+            )
+        );
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo json_encode(
+            array(
+                "status" => 0,
+                "data" => array(),
+                "msg" => "Sorry, there was an error uploading your file."
+            )
+        );
     }
 }
